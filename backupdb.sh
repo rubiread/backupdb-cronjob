8f2f8e8c9d023b59b322636bc823561aff2b7664
RubiRead <ruwan@wso2telco.com>
2016-07-14 15:06:24 +0530

    backup_db.sh

---
+++ backupdb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+db_host=localhost
+db_name=database
+db_user=root
+db_passwd=pass
+
+ftp_host=remote_ip
+ftp_user=ftp_user_name
+ftp_passwd=ftp_password
+ftp_dir=/home/
+
+
+backup_file_name=$(date +"%Y-%b-%d-%H:%M")_$db_name;
+
+
+mysqldump --single-transaction --user=$db_user --password=$db_passwd --host=$db_host $db_name > $backup_file_name.sql
+
+
+ftp -n -v $ftp_host << EOT
+ascii
+prompt
+user $ftp_user $ftp_passwd
+mput *_$db_name.sql $ftp_dir
+bye
+EOT
+
+
+rm *_$db_name.sql
