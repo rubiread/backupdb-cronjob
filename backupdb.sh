42f24a4431d3a41c8ea5e86b953c3066a87d92a7
RubiRead <ruwan@wso2telco.com>
2016-07-14 15:14:39 +0530

    comment updated with triggers and stored proceedure dump

--- backupdb.sh
+++ backupdb.sh
@@ -13,6 +13,9 @@ ftp_dir=/home/
 
 backup_file_name=$(date +"%Y-%b-%d-%H:%M")_$db_name;
 
+# DUMP DB
+# Add --routines for dump stored proceedures
+# Add --triggers  for dump triggers 
 
 mysqldump --single-transaction --user=$db_user --password=$db_passwd --host=$db_host $db_name > $backup_file_name.sql
 
